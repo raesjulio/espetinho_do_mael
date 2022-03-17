ae2bff15b9809f764a71ef1d2958d7581bfd5125
Julio <despairnatural@gmail.com>
2022-03-17 16:39:46 -0400

    Criação de consulta ao cardapio

--- src/pages/cardapio.tsx
+++ src/pages/cardapio.tsx
@@ -1,10 +1,42 @@
+import axios from "axios";
+import { GetStaticProps } from "next";
 import { HomeCardapio } from "../components/Cardapio/HomeCardapio";
+import { supabase } from "../utils/supabase";
+type ICardapio = {
+  id: Number
+  nome: string
+  descricao: string
+  preco: Number
+  disponivel: boolean
+}
+interface List {
+  listProduct:[{
+    id: Number
+    nome: string
+    descricao: string
+    preco: Number
+    disponivel: boolean
+  }]
+}
 
+export default function Cardapio({ listProduct}: List) {
+  console.log(listProduct);
+  
+  return (
+    <>
+      <HomeCardapio />
+    </>
+  )
+}
 
-export default function Cardapio() {
-    return (
-      <>
-        <HomeCardapio/>
-      </>
-    )
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await supabase.from<ICardapio[]>("cardapio").select("*").then(response => {
+    return response.body
+  })
+  return {
+    props: {
+      listProduct: data
+    },
+    revalidate: 60 * 60 * 24, //24horas
   }
+}
