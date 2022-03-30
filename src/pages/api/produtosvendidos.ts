ba7f03302bdacd13c6d3b3118b3f437e9974d3c1
Julio <despairnatural@gmail.com>
2022-03-29 22:52:04 -0400

    Nova rota

--- src/pages/api/produtosvendidos.ts
+++ src/pages/api/produtosvendidos.ts
@@ -3,10 +3,10 @@ import { supabase } from "../../utils/supabase";
 
 export default async (request: NextApiRequest, response: NextApiResponse) =>{
     if (request.method === "GET") {
-        const { data, error, count } = await supabase
-        .from('pedido').select("total",{count: "exact"}).gt('status', "2")
-        console.log(count);
-        
-        return response.status(200).json(data)
+        const { data, error } = await supabase.rpc("get_list_produt_vendidos")
+        if (data) {
+            return response.status(200).json(data)
+        }
+        return response.status(400).json(error)
     }
 }
---
+++ src/pages/dashboard/produtosvendidos.tsx
@@ -0,0 +1,13 @@
+import { HomePedidos } from "../../components/HomePedidos/HomePedidos";
+
+export default function ProdutosVendidos() {
+    
+    return (
+        <>
+
+       
+
+
+        </>
+    )
+}
